18eacc1ccf97dee81460d0c6687c90ae5db6051f
Weilbyte <me@weilbyte.dev>
2021-12-24 02:55:19 +0100

    Remove ruser
    sshd doesnt seem to provide this

--- src/main.c
+++ src/main.c
@@ -12,13 +12,11 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const c
 
     char *user;
     char *rhost;
-    char *ruser;
     char *service;
 
     (void) pam_get_item(pamh, PAM_USER, &user);
     (void) pam_get_item(pamh, PAM_SERVICE, &service);
     (void) pam_get_item(pamh, PAM_RHOST, &rhost);
-    (void) pam_get_item(pamh, PAM_RUSER, &ruser);
     
     if (argv[0] && strstr(service, "sshd")) {
         curl_global_init(CURL_GLOBAL_ALL);
